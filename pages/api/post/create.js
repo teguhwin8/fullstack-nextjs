f5fc1edec84ba55351bd0959a16333cb79867711
Teguh Widodo <teguhwin8@gmail.com>
2021-02-26 02:49:00 +0700

    auth on posts api

--- pages/api/post/create.js
+++ pages/api/post/create.js
@@ -1,8 +1,11 @@
 import db from "../../../libs/db"
+import authorization from "../../../middlewares/authorization"
 
 export default async function handler(req, res) {
     // Check request method
     if (req.method !== 'POST') return res.status(405).end()
+
+    const auth = await authorization(req, res)
 
     // Define req variable
     const { title, content } = req.body
--- pages/api/post/index.js
+++ pages/api/post/index.js
@@ -1,7 +1,10 @@
 import db from "../../../libs/db"
+import authorization from "../../../middlewares/authorization"
 
 export default async function handler(req, res) {
     if (req.method !== 'GET') return res.status(405).end()
+
+    const auth = await authorization(req, res)
 
     const posts = await db('posts')
 
@@ -10,4 +13,5 @@ export default async function handler(req, res) {
         message: "Posts data",
         data: posts
     })
+
 }
